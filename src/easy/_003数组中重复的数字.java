c3d468da74131a71ee6cea30754eb4fe70777690
zhaotianhao <zhaotianhao@bytedance.com>
2023-05-10 11:27:56 +0800

    feat:findRepeatNumber

--- src/easy/_003数组中重复的数字.java
+++ src/easy/_003数组中重复的数字.java
@@ -1,5 +1,10 @@
 package easy;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 /*
 找出数组中重复的数字。
 在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。
@@ -15,7 +20,12 @@ package easy;
 */
 class _003数组中重复的数字 {
     public int findRepeatNumber(int[] nums) {
-
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num)) {
+                return num;
+            }
+        }
         return 0;
     }
 }
