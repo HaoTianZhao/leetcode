662f85db2df11c7241c8e0c4a41e53346423a05d
zhaotianhao <zhaotianhao@bytedance.com>
2023-05-16 19:12:27 +0800

    feat:2023.05.16

--- src/剑指offer第二版/medium/_007重建二叉树.java
+++ src/剑指offer第二版/medium/_007重建二叉树.java
@@ -1,5 +1,7 @@
 package 剑指offer第二版.medium;
 
+import java.util.HashMap;
+
 /*
 输入某二叉树的前序遍历和中序遍历的结果，请构建该二叉树并返回其根节点。
 假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
@@ -17,5 +19,56 @@ Output: [-1]
 
  */
 public class _007重建二叉树 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
 
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    int index = 0;
+    HashMap<Integer, Integer> value2IndexMap = new HashMap<>();
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder.length == 0) {
+            return null;
+        }
+
+        // 构建inorder值到下标的map, 递归时不用循环查找
+        for (int idx = 0; idx < inorder.length; idx++) {
+            value2IndexMap.put(inorder[idx], idx);
+        }
+
+        int val = preorder[index];
+        TreeNode tree = new TreeNode(val);
+        for (int i = 0; i < inorder.length; i++) {
+            if (val == inorder[i]) {
+                index++;
+                // index是preorder的游标. 此时[0-i)是树的左侧, [i,end)是树的右侧, 进行递归
+                tree.left = buildTree(preorder, inorder, 0, i);
+                tree.right = buildTree(preorder, inorder, i + 1, inorder.length);
+                return tree;
+            }
+        }
+        return tree;
+    }
+
+    TreeNode buildTree(int[] preorder, int[] inorder, int left, int right) {
+        if (left > preorder.length || index > preorder.length - 1) {
+            return null;
+        }
+        int val = preorder[index];
+        Integer i = value2IndexMap.get(val);
+        if (i == null || i < left || i >= right) {
+            return null;
+        }
+        index++;
+        TreeNode node = new TreeNode(val);
+        node.left = buildTree(preorder, inorder, left, i);
+        node.right = buildTree(preorder, inorder, i + 1, right);
+        return node;
+    }
 }
